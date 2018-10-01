f4685a30845cd5378555b4868cc88a513778fbf4
Aventerine <aventerine@gmail.com>
2018-10-01 15:46:09 -0400

    fixed none being default

--- src/components/SelectShelf.js
+++ src/components/SelectShelf.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 class SelectShelf extends Component {
   state = {
-    value: 'none'
+    value: 'None'
   }
 
   static propTypes = {
@@ -27,11 +27,11 @@ class SelectShelf extends Component {
   render() {
     return (<div className="book-shelf-changer">
       <select value={this.state.value} onChange={this.handleChange}>
-        <option value="none" disabled="disabled">Move to...</option>
+        <option value="none" disabled>Move to...</option>
+        <option value="none" selected>None</option>
         <option value="currentlyReading">Currently Reading</option>
         <option value="wantToRead">Want to Read</option>
         <option value="read">Read</option>
-        <option value="none">None</option>
       </select>
     </div>)
   }
